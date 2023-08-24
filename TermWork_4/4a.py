25ded835c09c62279f99b396a288852fbdc49540
Gajendra Deshpande <gcdeshpande@hotmail.com>
2023-08-24 11:32:35 +0530

    Update 4a.py

--- TermWork_4/4a.py
+++ TermWork_4/4a.py
@@ -1,1 +1,56 @@
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left_half = arr[:mid]
+        right_half = arr[mid:]
+
+        merge_sort(left_half)
+        merge_sort(right_half)
+
+        i = j = k = 0
+
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                arr[k] = left_half[i]
+                i += 1
+            else:
+                arr[k] = right_half[j]
+                j += 1
+            k += 1
+
+        while i < len(left_half):
+            arr[k] = left_half[i]
+            i += 1
+            k += 1
+
+        while j < len(right_half):
+            arr[k] = right_half[j]
+            j += 1
+            k += 1
+
+# Example usage
+n =int(input("Enter the number of elements in the list: "))
+
+input_list=[]
+for i in range(n):
+  input_list.append(int(input(f"Enter element {i}: ")))
+
+print("Original list:", input_list)
+
+insertion_sorted = input_list.copy()
+insertion_sort(insertion_sorted)
+print("Insertion sorted:", insertion_sorted)
+
+merge_sorted = input_list.copy()
+merge_sort(merge_sorted)
+print("Merge sorted:", merge_sorted)
 
